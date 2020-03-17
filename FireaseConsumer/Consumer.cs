a1e7c33c2f6104c3849b77872905dfdaf921041f
Zhiru Cao <1074160@ucn.dk>
2020-03-17 15:00:34 +0100

    cusmer firebase

---
+++ FireaseConsumer/Consumer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using DataModels;
+
+namespace FirebaseConsumer {
+    class Consumer {
+        static void Main(string[] args) {
+            using (var client = new HttpClient()) {
+                client.BaseAddress = new Uri("https://us-central1-mapspeople-45db3.cloudfunctions.net/respond");
+                var responseTask = client.GetAsync("source");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode) {
+                    var readTask = result.Content.ReadAsAsync<Source[]>();
+                    readTask.Wait();
+
+                    var sources = readTask.Result;
+                    foreach (var source in sources) {
+                        Console.WriteLine(source.TimeStamp);
+                    }
+
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ FirebaseConsumer2/ConsumerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseConsumer2 {
+    class ConsumerModel {
+        public string Id { get; set; }
+        public string State { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
