b6114ad9efdf03ae6bd4d3c6c884fcf28b3825f1
Emil Hedeholm Sørensen <1074195@ucn.dk>
2020-03-17 12:53:25 +0100

    added internal datamodel

---
+++ DataModels/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class Location
+    {
+        public string Id { get; set; }
+        public string Parent { get; set; }
+        public IEnumerable<Source> Sources { get; set; }
+    }
+}
---
+++ DataModels/Source.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels {
+    public class Source {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public List<string> State { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
